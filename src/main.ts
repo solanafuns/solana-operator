d47c1cafe7a208e8065d22b76a84a1818051c736
v1xingyue <qixingyue@gmail.com>
2023-09-13 08:23:27 +0800

    add gitpod

---
+++ src/main.ts
@@ -0,0 +1,53 @@
+import { initializeKeypair } from "./init";
+import * as web3 from "@solana/web3.js";
+import {
+  createMint,
+  TOKEN_PROGRAM_ID,
+  getOrCreateAssociatedTokenAccount,
+} from "@solana/spl-token";
+
+const connection = new web3.Connection("http://127.0.0.1:8899");
+// const connection = new web3.Connection(web3.clusterApiUrl("devnet"));
+
+const main = async () => {
+  const pair = await initializeKeypair(connection);
+  const newToken = web3.Keypair.generate();
+  const address = await createMint(
+    connection,
+    pair,
+    pair.publicKey,
+    pair.publicKey,
+    5,
+    newToken,
+    undefined,
+    TOKEN_PROGRAM_ID
+  );
+  console.log(`token address : ${address}`);
+
+  const myAccount = await getOrCreateAssociatedTokenAccount(
+    connection,
+    pair,
+    address,
+    pair.publicKey
+  );
+  console.log(`my account : ${myAccount.address}`);
+
+  const myAccountAgain = await getOrCreateAssociatedTokenAccount(
+    connection,
+    pair,
+    address,
+    pair.publicKey
+  );
+
+  console.log(`my account : ${myAccountAgain.address}`);
+};
+
+main()
+  .then(() => {
+    console.log("Finished successfully");
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
